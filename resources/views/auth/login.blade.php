73fec6cf2c6f53545aead3af5ae02e76e43de67a
Marco Mendes <marcomendes0202@hotmail.com>
2017-12-15 09:31:52 +0000

    change header
    change header

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     
 <head>
-        <title>Frota Login</title><meta charset="UTF-8" />
+        <title>Frota Mais - Gestão Automóvel</title><meta charset="UTF-8" />
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Frota+">
--- resources/views/include/head.blade.php
+++ resources/views/include/head.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>Frota +</title>
+    <title>Frota Mais - Gestão Automóvel</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Frota+">
